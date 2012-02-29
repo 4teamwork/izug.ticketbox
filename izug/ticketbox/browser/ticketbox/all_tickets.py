4efd96dcb7815ce37debac55886f684ce02d5f0a
Jonas Baumann <jone@jone.ch>
2012-02-29 17:29:42 +0100

    Remove old tabbedview implementation.

--- izug/ticketbox/browser/ticketbox/all_tickets.py
+++
@@ -1,9 +0,0 @@
-from baseview import TabbedTicketBoxBaseView
-
-
-class TicketBoxViewAllTickets(TabbedTicketBoxBaseView):
-
-    types = 'Ticket'
-    #this is a attribute in the DataGrid States from TicketBox ContentType
-    filter_state = "show_in_all_tickets"
-    columns = None
