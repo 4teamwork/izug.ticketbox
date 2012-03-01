c1bdf38eb97f926abed19eab03f45b6d89a11b58
Jonas Baumann <jone@jone.ch>
2012-03-01 11:21:52 +0100

    Cleanup ticketbox.py

--- izug/ticketbox/content/ticketbox.py
+++ izug/ticketbox/content/ticketbox.py
@@ -18,135 +18,142 @@ from zope.interface import implements
 
 TicketBoxSchema = folder.ATBTreeFolderSchema.copy() + Schema((
 
-    #Individual Identifier
-    StringField(
-        name='individualIdentifier',
-        searchable=True,
-        widget=StringWidget(
-            label=_(
-                u"label_individualIdentifier",
-                default=u"Individual identifier"),
-            description=_(
-                u"help_individualIdentifier",
-                default=u"Enter a individual identifier (max 7 positions)"),
-            maxlength=7,
-        ),
-    ),
+        #Individual Identifier
+        StringField(
+            name='individualIdentifier',
+            searchable=True,
 
-    #Available States
-    DataGridField(
-        name='availableStates',
-        searchable=True,
-        allow_empty_rows=False,
-        default=(
-            {'id': '',
-             'title': "offen",
-             'show_in_all_tickets': '1',
-             'show_in_my_tickets': '1',
-            },
-            {'id': '',
-             'title': "in Bearbeitung",
-             'show_in_all_tickets': '1',
-             'show_in_my_tickets': '1',
-            },
-            {'id': '',
-             'title': "zur\xc3\xbcckgewiesen",
-             'show_in_all_tickets': '1',
-             'show_in_my_tickets': '1',
-            },
-            {'id': '',
-             'title': "zum Testen",
-             'show_in_all_tickets': '1',
-             'show_in_my_tickets': '1',
-            },
-            {'id': '',
-             'title': "erledigt",
-             'show_in_all_tickets': '1',
-             'show_in_my_tickets': '0',
-            },
-            {'id': '',
-             'title': "verschoben",
-             'show_in_all_tickets': '1',
-             'show_in_my_tickets': '1',
-            },
-        ),
-        widget=DataGridWidget(
-            visible={'view': 'invisible', 'edit': 'visible'},
-            label=_(u"Define states"),
-            description=_(u"add or delete possible state-information"),
-            columns={
-                'id': Column(_(u"id")),
-                'title': Column(_(u"title")),
-                'show_in_all_tickets': SelectColumn(
-                    _(u"show in 'all tickets'"),
-                    # DatagridFields SelectColumn doesn not behave like an
-                    # archetype field. It always does
-                    # a getattr to get the vocab.
-                    vocabulary="yes_no"),
-                'show_in_my_tickets': SelectColumn(
-                    _(u"show in 'my tickets'"),
-                    vocabulary="yes_no"),
-            }
-        ),
-        columns=("id", "title", "show_in_all_tickets", "show_in_my_tickets"),
-    ),
+            widget=StringWidget(
+                label=_(u"label_individualIdentifier",
+                        default=u"Individual identifier"),
+                description=_(u"help_individualIdentifier",
+                              default=u"Enter a individual identifier " + \
+                                  u"(max 7 positions)"),
+                maxlength=7,
+                )),
 
-    #Available Releases
-    DataGridField(
-        name='availableReleases',
-        widget=DataGridWidget(
-            visible={'view': 'invisible', 'edit': 'visible'},
-            label=_(u'label_availableReleases',default=u"Available Releases"),
-            description=_(u'help_avialableReleases',
-                default=u"Enter the Available Releases for this tracker."),
-            column_names=(_(u'Releases_id'), _(u'Releases_title')),
-        ),
-        allow_empty_rows=False,
-        columns=('id', 'title')
-    ),
+        #Available States
+        DataGridField(
+            name='availableStates',
+            searchable=True,
+            allow_empty_rows=False,
 
-    #Available Priorities
-    DataGridField(
-        name='availablePriorities',
-        widget=DataGridWidget(
-            visible={'view': 'invisible', 'edit': 'visible'},
-            label=_(u'label_availablePriorities', u"Available priorities"),
-            description=(_(u'help_availablePriorities',
-                default=u"Enter the different type of issue severities" +
-                " that should be available, one per line.")
-            ),
-            column_names=(
-                _(u'Priorities_id'),
-                _(u'Priorities_title')),
-            ),
-        allow_empty_rows=False,
-        columns=('id', 'title'),
-    ),
+            default=(
+                {'id': '',
+                 'title': "offen",
+                 'show_in_all_tickets': '1',
+                 'show_in_my_tickets': '1',
+                 },
 
-    #Available Areas
-    DataGridField(
-        name='availableAreas',
-        widget=DataGridWidget(
-            visible={'view': 'invisible', 'edit': 'visible'},
-            label=_(u'label_Areas',default=u"Areas"),
-            description=(
-                _(u'help_areas',default=u"Enter the issue topics/areas for this tracker.")
-            ),
-            column_names=(
-                _(u'Areas_id'),
-                _(u'Areas_title')),
-            ),
-        allow_empty_rows=False,
-        columns=('id', 'title'),
-    ),
-))
+                {'id': '',
+                 'title': "in Bearbeitung",
+                 'show_in_all_tickets': '1',
+                 'show_in_my_tickets': '1',
+                 },
+
+                {'id': '',
+                 'title': "zur\xc3\xbcckgewiesen",
+                 'show_in_all_tickets': '1',
+                 'show_in_my_tickets': '1',
+                 },
+
+                {'id': '',
+                 'title': "zum Testen",
+                 'show_in_all_tickets': '1',
+                 'show_in_my_tickets': '1',
+                 },
+
+                {'id': '',
+                 'title': "erledigt",
+                 'show_in_all_tickets': '1',
+                 'show_in_my_tickets': '0',
+                 },
+
+                {'id': '',
+                 'title': "verschoben",
+                 'show_in_all_tickets': '1',
+                 'show_in_my_tickets': '1',
+                 },
+                ),
+            columns=("id", "title",
+                     "show_in_all_tickets", "show_in_my_tickets"),
+
+            widget=DataGridWidget(
+                visible={'view': 'invisible', 'edit': 'visible'},
+                label=_(u"Define states"),
+                description=_(u"add or delete possible state-information"),
+                columns={
+                    'id': Column(_(u"id")),
+                    'title': Column(_(u"title")),
+                    'show_in_all_tickets': SelectColumn(
+                        _(u"show in 'all tickets'"),
+                        # DatagridFields SelectColumn doesn not behave like an
+                        # archetype field. It always does
+                        # a getattr to get the vocab.
+                        vocabulary="yes_no"),
+                    'show_in_my_tickets': SelectColumn(
+                        _(u"show in 'my tickets'"),
+                        vocabulary="yes_no"),
+                    })),
+
+        #Available Releases
+        DataGridField(
+            name='availableReleases',
+            allow_empty_rows=False,
+            columns=('id', 'title'),
+
+            widget=DataGridWidget(
+                visible={'view': 'invisible', 'edit': 'visible'},
+                label=_(u'label_availableReleases',
+                        default=u"Available Releases"),
+                description=_(u'help_avialableReleases',
+                              default=u"Enter the Available Releases " + \
+                                  u"for this tracker."),
+                column_names=(_(u'Releases_id'), _(u'Releases_title')))),
+
+        #Available Priorities
+        DataGridField(
+            name='availablePriorities',
+            allow_empty_rows=False,
+            columns=('id', 'title'),
+
+            widget=DataGridWidget(
+                visible={'view': 'invisible', 'edit': 'visible'},
+                label=_(u'label_availablePriorities',
+                        u"Available priorities"),
+                description=_(u'help_availablePriorities',
+                               default=u"Enter the different type of " + \
+                                  u"issue severities that should be " + \
+                                  u"available, one per line."),
+                column_names=(
+                    _(u'Priorities_id'),
+                    _(u'Priorities_title')))),
+
+        #Available Areas
+        DataGridField(
+            name='availableAreas',
+            allow_empty_rows=False,
+            columns=('id', 'title'),
+
+            widget=DataGridWidget(
+                visible={'view': 'invisible', 'edit': 'visible'},
+                label=_(u'label_Areas', default=u"Areas"),
+                description=_(u'help_areas',
+                              default=u"Enter the issue topics/areas" + \
+                                  u" for this tracker."),
+                column_names=(
+                    _(u'Areas_id'),
+                    _(u'Areas_title')),
+                )),
+
+        ))
 
 
 schemata.finalizeATCTSchema(
     TicketBoxSchema,
     folderish=True,
     moveDiscussion=False,
-)
+    )
 
 
 class TicketBox(folder.ATBTreeFolder):
@@ -173,8 +180,8 @@ class TicketBox(folder.ATBTreeFolder):
         contents: yes and no
         """
         return DisplayList((
-            ("1", _(u"yes")),
-            ("0", _(u"no")),
-            ))
+                ("1", _(u"yes")),
+                ("0", _(u"no")),
+                ))
 
 registerType(TicketBox, PROJECTNAME)
