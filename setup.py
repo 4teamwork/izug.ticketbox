c6c8794d7edd9ddadb3cbce16579752daf0f1a7d
Jonas Baumann <jone@jone.ch>
2012-03-01 11:15:26 +0100

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-This module contains the tool of izug.ticketbox
-"""
 from setuptools import setup, find_packages
 import os
 
 version = '4.1dev'
 maintainer = 'Elio Schmutz'
-tests_require = ['zope.testing']
+
+tests_require = [
+    'zope.testing',
+    ]
 
 setup(name='izug.ticketbox',
       version=version,
-      maintainer=maintainer,
-      description="A tracker-like task management system",
-        long_description = (open('README.rst').read() + '\n' + \
-                  open(os.path.join('docs', 'HISTORY.txt')).read()),
+      description="A tracker-like task management system for plone.",
+      long_description=(open('README.rst').read() + '\n' + \
+                            open(os.path.join('docs', 'HISTORY.txt')).read()),
+
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
@@ -26,27 +25,33 @@ setup(name='izug.ticketbox',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Topic :: Software Development :: Libraries :: Python Modules', ],
+
       keywords='ticketbox tracker ftw ',
       author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
+      maintainer=maintainer,
       url='https://github.com/4teamwork/izug.ticketbox',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['izug', ],
       include_package_data=True,
       zip_safe=False,
-      install_requires=['setuptools',
-                        'Products.DataGridField',
-                        'ftw.tabbedview',
-                        'ftw.table',
-                        'ftw.notification.base',
-                        'ftw.notification.email',
-                        'izug.utils',
-                        'ftw.sendmail',
-                        # -*- Extra requirements: -*-
-                        ],
+
+      install_requires=[
+        'setuptools',
+        'Products.DataGridField',
+        'ftw.tabbedview',
+        'ftw.table',
+        'ftw.notification.base',
+        'ftw.notification.email',
+        'izug.utils',
+        'ftw.sendmail',
+        ],
+
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
+
       test_suite='izug.ticketbox.tests.test_docs.test_suite',
       entry_points="""
       # -*- entry_points -*-
