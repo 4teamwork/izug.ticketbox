2a21e711949985f661c2b7ab7ca9c7fedab4a6ca
Jonas Baumann <jone@jone.ch>
2012-10-08 14:14:12 +0200

    Simplify the upgrade step by using catalog_reindex_objects by ftw.upgrade.

--- izug/ticketbox/upgrades/to4100.py
+++ izug/ticketbox/upgrades/to4100.py
@@ -1,30 +1,9 @@
-from ftw.upgrade import ProgressLogger
 from ftw.upgrade import UpgradeStep
-import logging
-
-
-LOG = logging.getLogger('izug.ticketbox.upgrades')
 
 
 class AddTicketboxTitleIndex(UpgradeStep):
 
     def __call__(self):
-        self.import_catalog()
-        self.add_catalog_index()
-        self.add_update_catalog()
-
-    def import_catalog(self):
         self.setup_install_profile('profile-izug.ticketbox.upgrades:4100')
-
-    def add_catalog_index(self):
-        if not self.catalog_has_index('ticketbox_title'):
-            self.catalog_add_index('ticketbox_title', 'FieldIndex')
-
-    def add_update_catalog(self):
-        query = {'portal_type': 'Ticket'}
-        objects = self.catalog_unrestricted_search(query, full_objects=True)
-
-        with ProgressLogger('Reindex tickets', objects) as step:
-            for obj in objects:
-                obj.reindexObject(idxs=['ticketbox_title'])
-                step()
+        self.catalog_reindex_objects({'portal_type': 'Ticket'},
+                                     idxs=['ticketbox_title'])
