6f2e85cf3d088fb3b7a2a220cfcd2db9387a8376
Jonas Baumann <jone@jone.ch>
2012-12-04 11:31:14 +0100

    Remove Products.PloneTestCase.

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ version = '4.5.1.dev0'
 maintainer = 'Mathias Leimgruber'
 
 tests_require = [
-    'collective.testcaselayer',
-    'Products.PloneTestCase',
     'plone.app.testing',
     'unittest2',
     ]
