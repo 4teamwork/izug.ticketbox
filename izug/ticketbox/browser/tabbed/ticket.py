9d40aa2185cdc0f3d13db8730c0f706b4eef4cfe
Jonas Baumann <jone@jone.ch>
2014-09-08 15:58:17 +0200

    activate subtickets tab on tickets.

--- izug/ticketbox/browser/tabbed/ticket.py
+++ izug/ticketbox/browser/tabbed/ticket.py
@@ -1,14 +1,22 @@
+from ftw.tabbedview import tabbedviewMessageFactory
 from ftw.tabbedview.browser.tabbed import TabbedView
 from izug.ticketbox import ticketboxMessageFactory as _
+from izug.ticketbox.interfaces import ISubTicket
 
 
 class TicketTabbedView(TabbedView):
 
     def get_tabs(self):
-        return (
+        tabs = [
             {'id': _('ticket'),
              'class': ''},
 
             {'id': _('attachments'),
              'class': ''},
-            )
+            ]
+
+        if not ISubTicket.providedBy(self.context):
+            tabs.append({'id': tabbedviewMessageFactory('issued_subtickets'),
+                         'class': ''})
+
+        return tabs
