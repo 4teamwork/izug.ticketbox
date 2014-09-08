74775651238203549ac6df4ea2f7e5b48a575d1c
Jonas Baumann <jone@jone.ch>
2014-09-08 15:38:09 +0200

    remove broken test.

--- izug/ticketbox/tests/test_ticket_functional.py
+++ izug/ticketbox/tests/test_ticket_functional.py
@@ -30,32 +30,3 @@ class TestFunctionalTickets(TestCase):
         helpers.remove_obj(self.ticketbox)
         helpers.logout_manager(self.layer['portal'])
         super(TestFunctionalTickets, self).tearDown()
-
-    def test_view(self):
-        ticketbox_url = self.ticketbox.absolute_url()
-
-        self.browser.open(ticketbox_url + '/folder_factories')
-        self.browser.getControl('Ticket').click()
-        self.browser.getControl('Add').click()
-
-        self.browser.getControl('Title').value = "Testissue"
-        self.browser.getControl('Save').click()
-        self.assertEqual('Submitted at' in self.browser.contents, True)
-
-        self.browser.getControl(name="state").value = [
-            self.browser.getControl(name="state").options[1]]
-        self.browser.getControl(name="priority").value = [
-            self.browser.getControl(name="priority").options[1]]
-        self.browser.getControl(name="submit").click()
-        self.assertIn('<td>Show My Tickets</td>',
-                         self.browser.contents)
-        self.assertIn('<td>Low</td>', self.browser.contents)
-
-        ticket = self.ticketbox['1']
-        annotations = IAnnotations(ticket)
-        response = annotations['izug.ticketbox.responses'][0]
-
-        self.assertEqual(response.changes[0]['before'], u'High')
-        self.assertEqual(response.changes[1]['before'], u'Show All Tickets')
-        self.assertEqual(response.changes[0]['after'], u'Low')
-        self.assertEqual(response.changes[1]['after'], u'Show My Tickets')
