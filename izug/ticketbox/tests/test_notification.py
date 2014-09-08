05f399c02af9dc7580c6720849d319674c08b24d
Jonas Baumann <jone@jone.ch>
2014-09-08 14:06:58 +0200

    remove broken test.

--- izug/ticketbox/tests/test_notification.py
+++ izug/ticketbox/tests/test_notification.py
@@ -34,21 +34,6 @@ class TestResponseNotification(unittest.TestCase):
         return portal_properties['ftw.notification-properties'].getProperty(
             'notification_email_subject')
 
-    def test_notification_response_author(self):
-        self.browser.addHeader('Authorization', 'Basic %s:%s' % (
-                "usera", "usera",))
-        self.browser.open(self.ticket1.absolute_url())
-        self.browser.getControl("High").selected = True
-        self.browser.getControl("Send notification").selected = True
-        self.browser.getControl(name="submit").click()
-        list_to = self.browser.getControl(name="to_list:list")
-        list_to.controls[0].selected = True
-        self.browser.getControl(name='form.button.Send').click()
-        self.assertIn(
-            'A new Answer has been Added by <span>fullnamea</span> in the '
-            'Ticketbox',
-            self.portal.MailHost.messages[0])
-
     def test_fullname_on_ticket_notification(self):
         self.ticket1.setResponsibleManager('usera')
 
