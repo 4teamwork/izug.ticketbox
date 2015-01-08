cc9ddb7203852153b599acca7bb1f04088e94a55
Martin Bächtold <github@tph.ch>
2015-01-08 15:38:46 +0100

    Activate select2 widget for more form fields.

--- izug/ticketbox/browser/javascripts/ticketresponse.js
+++ izug/ticketbox/browser/javascripts/ticketresponse.js
@@ -1,4 +1,6 @@
 $(document).on('ready reload', function() {
   $('select[name=responsibleManager]').select2();
   $('select[name=issuer]').select2();
+  $('select[name=variety]').select2();
+  $('select[name=area]').select2();
 });
