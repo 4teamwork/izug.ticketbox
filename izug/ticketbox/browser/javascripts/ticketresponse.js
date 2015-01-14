ffb4993df257ac51fa8d7a30bf1904bb4b7c838e
Martin Bächtold <github@tph.ch>
2015-01-14 17:07:09 +0100

    Set auto-width on select2 fields.

--- izug/ticketbox/browser/javascripts/ticketresponse.js
+++ izug/ticketbox/browser/javascripts/ticketresponse.js
@@ -1,6 +1,6 @@
 $(document).on('ready reload', function() {
-  $('select[name=responsibleManager]').select2();
-  $('select[name=issuer]').select2();
-  $('select[name=variety]').select2();
-  $('select[name=area]').select2();
+  $('select[name=responsibleManager]').select2({dropdownAutoWidth: 'true'});
+  $('select[name=issuer]').select2({dropdownAutoWidth: 'true'});
+  $('select[name=variety]').select2({dropdownAutoWidth: 'true'});
+  $('select[name=area]').select2({dropdownAutoWidth: 'true'});
 });
